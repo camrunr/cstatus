03e01c2211ad570b36f46b6c5fa080cd01c7ba81
Jon Rust <wonderdog@rusts.net>
2019-09-23 07:28:46 -0700

    init

---
+++ cstatus.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+TEMPFILE=/tmp/cstatus.tmp
+HEADFILE=/tmp/cstatushead.tmp
+BODYFILE=/tmp/cstatusbody.tmp
+
+RED='\033[0;31m'
+YEL='\033[1;33m'
+CYA='\033[0;36m'
+GRE='\033[0;32m'
+NC='\033[0m'
+TAB='\t'
+
+splunk show cluster-status > $TEMPFILE
+sed -ne "/Replication/,/^$/ p" < $TEMPFILE | paste - - - - - | egrep -o "[A-Z].*" > $HEADFILE
+sed -ne "/$TAB.*$TAB/,$ p" < $TEMPFILE | egrep -v '^$' | paste - - - - | sort > $BODYFILE
+sed -ne "/Peers restarting/,/^$/ p" < $TEMPFILE > /tmp/cstatus.restarting.tmp
+sed -ne "/Peers to be rest/,/^$/ p" < $TEMPFILE > /tmp/cstatus.toberestarted.tmp
+sed -ne "/Peers restarted/,/^$/ p" < $TEMPFILE > /tmp/cstatus.restarted.tmp
+
+# if things are not good, color the first line of output RED or YELLOW
+if egrep -q "(not searchable)|(Ready.*NO)" $HEADFILE
+then
+    COLOR=$RED
+elif grep -q "not met" $HEADFILE
+then
+    COLOR="$YEL"
+else
+    COLOR="$GRE"
+fi
+# print the line with color combo, then reset to no color
+echo -ne $COLOR
+cat $HEADFILE | sed -e 's/Replication factor /RF=/' | \
+                sed -e 's/Search factor /SF=/' | \
+                sed -e 's/All data is //' | \
+                sed -e 's/Ready /Ready:/' 
+echo -ne $NC
+
+# for each peer, parse the line and dump it with color decor if needed
+while read -a LINE
+do
+    PEER="${LINE[0]}"
+    GUID="${LINE[1]}"
+    SEARCHABLE="${LINE[4]}"
+    STATUS="${LINE[6]}"
+    BUCKET="${LINE[8]}"
+
+    if ! echo $STATUS | egrep -q "Up"
+    then 
+        STATUS_COLOR="$YEL"
+    else
+        STATUS_COLOR="$GRE"
+    fi
+
+    if ! echo $SEARCHABLE | egrep -q "YES"
+    then 
+        SRCH_COLOR="$YEL"
+    else
+        SRCH_COLOR="$GRE"
+    fi
+
+    echo -ne "${PEER}${TAB}Status:${STATUS_COLOR}${STATUS}${NC}${TAB}Searchable:${SRCH_COLOR}${SEARCHABLE}${NC}${TAB}${GUID}${TAB}${BUCKET}\n"
+done < $BODYFILE
+
+if grep -q Peers /tmp/cstatus.toberestarted.tmp
+then
+    echo 
+    echo "Servers need to restart:     $(wc -l /tmp/cstatus.toberestarted.tmp)"
+    echo "Server currently restarting: $(cut -f 3 < /tmp/cstatus.restarting.tmp)"
+fi
