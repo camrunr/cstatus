3f8b59bed9ed9805a235640fa78a84191d008f81
Jon Rust <wonderdog@rusts.net>
2019-09-23 08:26:00 -0700

    Added debug flag, fixed restarting state

--- cstatus.sh
+++ cstatus.sh
@@ -2,6 +2,7 @@
 TEMPFILE=/tmp/cstatus.tmp
 HEADFILE=/tmp/cstatushead.tmp
 BODYFILE=/tmp/cstatusbody.tmp
+DEBUG=$1
 
 RED='\033[0;31m'
 YEL='\033[1;33m'
@@ -10,18 +11,24 @@ GRE='\033[0;32m'
 NC='\033[0m'
 TAB='\t'
 
-splunk show cluster-status > $TEMPFILE
-sed -ne "/Replication/,/^$/ p" < $TEMPFILE | paste - - - - - | egrep -o "[A-Z].*" > $HEADFILE
+if [[ "$DEBUG" == "DEBUG" ]]
+then
+    echo running with tmp files already created
+else
+    splunk show cluster-status > $TEMPFILE
+fi
+
+sed -ne '/Replication/,/^$/ p' < $TEMPFILE | paste - - - - - | egrep -o "[A-Z].*" > $HEADFILE
 sed -ne "/$TAB.*$TAB/,$ p" < $TEMPFILE | egrep -v '^$' | paste - - - - | sort > $BODYFILE
-sed -ne "/Peers restarting/,/^$/ p" < $TEMPFILE > /tmp/cstatus.restarting.tmp
-sed -ne "/Peers to be rest/,/^$/ p" < $TEMPFILE > /tmp/cstatus.toberestarted.tmp
-sed -ne "/Peers restarted/,/^$/ p" < $TEMPFILE > /tmp/cstatus.restarted.tmp
+sed -ne '/Peers restarting/,/^$/ p' < $TEMPFILE > /tmp/cstatus.restarting.tmp
+sed -ne '/Peers to be rest/,/^$/ p' < $TEMPFILE > /tmp/cstatus.toberestarted.tmp
+sed -ne '/Peers restarted/,/^$/ p' < $TEMPFILE > /tmp/cstatus.restarted.tmp
 
 # if things are not good, color the first line of output RED or YELLOW
 if egrep -q "(not searchable)|(Ready.*NO)" $HEADFILE
 then
     COLOR=$RED
-elif grep -q "not met" $HEADFILE
+elif egrep -q "(not met)|(in progress)" $HEADFILE
 then
     COLOR="$YEL"
 else
@@ -61,9 +68,13 @@ do
     echo -ne "${PEER}${TAB}Status:${STATUS_COLOR}${STATUS}${NC}${TAB}Searchable:${SRCH_COLOR}${SEARCHABLE}${NC}${TAB}${GUID}${TAB}${BUCKET}\n"
 done < $BODYFILE
 
+echo
 if grep -q Peers /tmp/cstatus.toberestarted.tmp
 then
-    echo 
     echo "Servers need to restart:     $(wc -l /tmp/cstatus.toberestarted.tmp)"
-    echo "Server currently restarting: $(cut -f 3 < /tmp/cstatus.restarting.tmp)"
+fi
+
+if grep -q Peers /tmp/cstatus.restarting.tmp
+then
+    echo "Server currently restarting: $(awk '{print $3}' < /tmp/cstatus.restarting.tmp)"
 fi
